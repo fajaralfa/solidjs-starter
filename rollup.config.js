6538bd7f470659f24054ff975b6901e93eefbcb4
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2023-11-10 16:32:34 +0700

    restructure project

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,13 @@ import nodeResolve from '@rollup/plugin-node-resolve'
 
 /**@type {import("rollup").RollupOptions} */
 export default {
-    input: 'src/main.jsx',
+    input: 'src/resources/main.jsx',
     output: {
-        dir: 'dist/',
+        dir: 'dist/public/js/',
         format: 'esm',
     },
     plugins: [
-        babel({presets: ['solid'], babelHelpers: 'bundled'}),
-        nodeResolve()
+        babel({ presets: ['solid'], babelHelpers: 'bundled' }),
+        nodeResolve(),
     ]
 }
